d8eaf1f238d8de613d3d5739a3d44fec271ed7c3
Emerson Tanno <tannobrizotto@gmail.com>
2025-09-29 08:53:10 -0300

    :sparkles: feat(Player_Controller): adding player animation and dividing the code in minor functions

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
 
+    #region Variables
     [SerializeField] float moveSpeed = 5f;
     public Transform movePoint;
     public LayerMask collision;
-
     private Animator myAnimator;
+    #endregion
 
+    #region Core
     void Awake()
     {
         myAnimator = GetComponent<Animator>();
@@ -23,9 +25,41 @@ public class Player_Controller : MonoBehaviour
 
     void Update()
     {
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
+        MovePointer();
+
+        IdleAnimation();
+    }
+    #endregion
+
+
+    #region Animation
+    private void ActivateAnimation(string animation)
+    {
+        myAnimator.SetBool("walk_front", false);
+        myAnimator.SetBool("walk_back", false);
+        myAnimator.SetBool("walk_left", false);
+        myAnimator.SetBool("walk_right", false);
+
+        myAnimator.SetBool(animation, true);
+    }
+
+    private void IdleAnimation()
+    {
+        if (Vector3.Distance(transform.position, movePoint.position) <= .0f)
+        {
+            myAnimator.SetBool("walk_front", false);
+            myAnimator.SetBool("walk_back", false);
+            myAnimator.SetBool("walk_left", false);
+            myAnimator.SetBool("walk_right", false);
+        }
+    }
+    #endregion
+
+    #region Movement
+    private void MovePointer()
+    {
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
 
@@ -34,6 +68,14 @@ public class Player_Controller : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, collision))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    if (Input.GetAxisRaw("Horizontal") == 1)
+                    {
+                        ActivateAnimation("walk_right");
+                    }
+                    if (Input.GetAxisRaw("Horizontal") == -1)
+                    {
+                        ActivateAnimation("walk_left");
+                    }
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
@@ -41,14 +83,19 @@ public class Player_Controller : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, collision))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    if (Input.GetAxisRaw("Vertical") == 1)
+                    {
+                        ActivateAnimation("walk_back");
+                    }
+                    if (Input.GetAxisRaw("Vertical") == -1)
+                    {
+                        ActivateAnimation("walk_front");
+                    }
                 }
             }
 
-            myAnimator.SetBool("Walk_Front", false);
-        }
-        else
-        {
-            myAnimator.SetBool("Walk_Front", true);
+
         }
     }
+    #endregion
 }
