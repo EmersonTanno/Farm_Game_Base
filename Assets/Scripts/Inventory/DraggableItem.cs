bd4375a343470eeab4ed15a9a769ce85d8b849c3
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-01 10:51:08 -0300

    :sparkles: feat(InventorySolt): creating inventory slot script

---
+++ Assets/Scripts/Inventory/DraggableItem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public UnityEngine.UI.Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        transform.localPosition = Vector3.zero;
+        image.raycastTarget = true;
+    }
+
+
+}
