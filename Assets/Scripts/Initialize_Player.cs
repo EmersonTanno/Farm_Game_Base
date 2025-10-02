7a865943c4fe8af10d2650064782867dae3b7e07
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-02 00:08:50 -0300

    :sparkles: feat(Initialize_Player): initializing player with items

---
+++ Assets/Scripts/Initialize_Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize_Player : MonoBehaviour
+{
+    public Item item1;
+    public Item item2;
+    public Item item3;
+
+    void Start()
+    {
+        InventoryManager.Instance.AddItem(item1);
+        InventoryManager.Instance.AddItem(item2);
+        InventoryManager.Instance.AddItem(item3);
+        InventoryManager.Instance.AddItem(item3);
+        InventoryManager.Instance.AddItem(item3);
+    }
+}
