b25a19dd7d5ea0d0cce040ffbb062f61e3a38eb3
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-10 23:01:48 -0300

    :sparkles: feat(Status_controller): adding animation on gold counter

--- Assets/Scripts/Player/Status_Controller.cs
+++ Assets/Scripts/Player/Status_Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,7 @@ public class Status_Controller : MonoBehaviour
 {
     public static Status_Controller Instance;
     int gold = 0;
+    int goldT = 0;
     [SerializeField] TextMeshProUGUI goldText;
 
     void Awake()
@@ -16,11 +18,67 @@ public class Status_Controller : MonoBehaviour
     public void AddGold(int quantity)
     {
         gold += quantity;
-        UpdateGoldCanva();
+        StartCoroutine(GoldAnimation());
+    }
+
+    public void RemoveGold(int quantity)
+    {
+        gold -= quantity;
+        StartCoroutine(GoldAnimation());
     }
 
     private void UpdateGoldCanva()
     {
-        goldText.text = gold.ToString();
+        goldText.text = goldT.ToString();
     }
+
+    private IEnumerator GoldAnimation()
+    {
+        Vector3 originalPosition = goldText.transform.position;
+        float time = 0.01f;
+
+        float diff = Mathf.Abs(gold - goldT);
+
+        if (diff >= 500)
+            time = 0.001f;
+
+        float shakeIntensity = 2f;
+        float shakeSpeed = 30f;
+
+        if (gold > goldT)
+        {
+            for (int i = goldT; i < gold; i++)
+            {
+                goldT = i;
+                UpdateGoldCanva();
+
+                float offsetX = Mathf.Sin(Time.time * shakeSpeed) * shakeIntensity;
+                float offsetY = Mathf.Cos(Time.time * shakeSpeed * 1.3f) * shakeIntensity;
+                goldText.transform.position = originalPosition + new Vector3(offsetX, offsetY, 0f);
+
+                shakeIntensity = Mathf.Lerp(2f, 0f, (float)(i - goldT) / diff);
+
+                yield return new WaitForSeconds(time);
+            }
+        }
+        else
+        {
+            for (int i = goldT; i > gold; i--)
+            {
+                goldT = i;
+                UpdateGoldCanva();
+
+                float offsetX = Mathf.Sin(Time.time * shakeSpeed) * shakeIntensity;
+                float offsetY = Mathf.Cos(Time.time * shakeSpeed * 1.3f) * shakeIntensity;
+                goldText.transform.position = originalPosition + new Vector3(offsetX, offsetY, 0f);
+
+                shakeIntensity = Mathf.Lerp(2f, 0f, (float)(i - goldT) / diff);
+
+                yield return new WaitForSeconds(time);
+            }
+        }
+
+        goldText.transform.position = originalPosition;
+    }
+
 }
