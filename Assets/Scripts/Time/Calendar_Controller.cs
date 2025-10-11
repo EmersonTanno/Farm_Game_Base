4df0b34aa770cf117eed3fb8b42546f03608f206
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-11 00:14:03 -0300

    :sparkles: feat(Calendar_Controller): adding seasons to callendar

--- Assets/Scripts/Time/Calendar_Controller.cs
+++ Assets/Scripts/Time/Calendar_Controller.cs
@@ -1,5 +1,6 @@
 using System;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Calendar_Controller : MonoBehaviour
@@ -8,12 +9,15 @@ public class Calendar_Controller : MonoBehaviour
     //Day and Month
     public int day = 0;
     public int month = 0;
+    public int year = 0;
 
     //Canvas
     [SerializeField] TextMeshProUGUI daysText;
 
     //Event
     public static event Action OnDayChange;
+    public static event Action OnMonthChange;
+    public static event Action OnYearChange;
     #endregion
 
     void Awake()
@@ -34,15 +38,43 @@ public class Calendar_Controller : MonoBehaviour
     private void ChangeDay()
     {
         day++;
+        if(day > 30)
+        {
+            month++;
+            day = 1;
+            OnMonthChange?.Invoke();
+            if(month > 4)
+            {
+                month = 1;
+                OnYearChange?.Invoke();
+            }
+        }
         OnDayChange?.Invoke();
         UpdateCanvas();
     }
 
     private void UpdateCanvas()
     {
+        string monthName = "";
+        switch (month)
+        {
+            case 1:
+                monthName = "Ver";
+                break;
+            case 2:
+                monthName = "Out";
+                break;
+            case 3:
+                monthName = "Inv";
+                break;
+            case 4:
+                monthName = "Pri";
+                break;
+        }
         if (daysText != null)
         {
-            daysText.text = $"Data: {day:D2}/{month:D2}";
+            daysText.text = $"Data: {day:D2}/{monthName}";
         }
     }
 }
+
