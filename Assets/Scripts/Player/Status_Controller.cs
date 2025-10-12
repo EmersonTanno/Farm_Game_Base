a354c3bc50fcabd59384bc264b8d417fa9afd1e8
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-12 10:32:52 -0300

    :sparkles: feat(Status_Controller): adding lucky

--- Assets/Scripts/Player/Status_Controller.cs
+++ Assets/Scripts/Player/Status_Controller.cs
@@ -7,16 +7,39 @@ using UnityEngine;
 public class Status_Controller : MonoBehaviour
 {
     public static Status_Controller Instance;
-    int gold = 0;
-    int goldT;
+
+    #region Variables
+    //Gold
+    public int gold = 0;
+    private int goldT;
     [SerializeField] TextMeshProUGUI goldText;
 
+    //Lucky
+    private int lucky = 0;
+    private int dayLucky = 0;
+
+    #endregion
+
+    #region Core
     void Awake()
     {
         Instance = this;
         goldT = gold;
         UpdateGoldCanva();
     }
+
+    void OnEnable()
+    {
+        Calendar_Controller.OnDayChange += GetLuckyForDay;
+    }
+
+    void OnDisable()
+    {
+        Calendar_Controller.OnDayChange -= GetLuckyForDay;
+    }
+    #endregion
+
+    #region Gold
     public void AddGold(int quantity)
     {
         gold += quantity;
@@ -82,5 +105,23 @@ public class Status_Controller : MonoBehaviour
 
         goldText.transform.position = originalPosition;
     }
+    #endregion
 
+    #region Lucky
+    public int GetLucky()
+    {
+        return lucky;
+    }
+
+    private void GetLuckyForDay()
+    {
+        lucky -= dayLucky;
+        if (lucky < 0)
+        {
+            lucky = 0;
+        }
+        dayLucky = UnityEngine.Random.Range(0, 11);
+        lucky += dayLucky;
+    }
+    #endregion
 }
