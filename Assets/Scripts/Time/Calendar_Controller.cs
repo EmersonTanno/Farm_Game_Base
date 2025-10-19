c9e26d8c50c890bedddae9a257b8bb58a234682a
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-19 17:12:46 -0300

    :bug: bugfix(Callendar_Controller): fix month change season

--- Assets/Scripts/Time/Calendar_Controller.cs
+++ Assets/Scripts/Time/Calendar_Controller.cs
@@ -46,18 +46,19 @@ public class Calendar_Controller : MonoBehaviour
     private void ChangeDay()
     {
         day++;
+        OnDayChange?.Invoke();
         if(day > 30)
         {
             month++;
             day = 1;
-            OnMonthChange?.Invoke();
-            if(month > 4)
+            if (month > 4)
             {
                 month = 1;
                 OnYearChange?.Invoke();
             }
+            SetSeason();
+            OnMonthChange?.Invoke();
         }
-        OnDayChange?.Invoke();
         UpdateCanvas();
     }
 
@@ -71,7 +72,6 @@ public class Calendar_Controller : MonoBehaviour
                 monthName = "Ver";
                 if (UnityEngine.ColorUtility.TryParseHtmlString("#FFCEBF", out newColor))
                 {
-                    season = Season.Verao;
                     globalLight.color = newColor;
                 }
                 break;
@@ -103,6 +103,25 @@ public class Calendar_Controller : MonoBehaviour
         if (daysText != null)
         {
             daysText.text = $"Data: {day:D2}/{monthName}";
+        }
+    }
+
+    private void SetSeason()
+    {
+        switch(month)
+        {
+            case 1:
+                season = Season.Verao;
+                break;
+            case 2:
+                season = Season.Outono;
+                break;
+            case 3:
+                season = Season.Inverno;
+                break;
+            case 4:
+                season = Season.Primavera;
+                break;
         }
     }
 }
