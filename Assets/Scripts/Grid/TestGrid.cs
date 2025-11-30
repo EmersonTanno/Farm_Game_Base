317843c44480df7a0b33d4d124036b28f8a92940
Emerson Tanno <tannobrizotto@gmail.com>
2025-11-30 10:11:27 -0300

    :sparkles: feat(grid): start the implementation of new grid system

---
+++ Assets/Scripts/Grid/TestGrid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class TestGrid : MonoBehaviour
+{
+    private Grid grid;
+
+    void Start()
+    {
+        grid = new Grid(3, 4, 1f, new Vector3(0, 0));
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
