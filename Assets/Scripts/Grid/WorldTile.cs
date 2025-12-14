7ed912ed0a3b23c8f9a98981716dd06d56cb8c90
Emerson Tanno <tannobrizotto@gmail.com>
2025-12-14 00:20:06 -0300

    :sparkles: feat(WorldTiles): creating world tiles

---
+++ Assets/Scripts/Grid/WorldTile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Tiles/World Tile")]
+public class WorldTile : Tile
+{
+    public int id;
+}
