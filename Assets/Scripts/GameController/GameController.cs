affa7120f340842256195941e43e141a1e6755ee
Emerson Tanno <tannobrizotto@gmail.com>
2025-12-19 15:16:29 -0300

    :sparkles: feat(GameController): creating a game controller

---
+++ Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
