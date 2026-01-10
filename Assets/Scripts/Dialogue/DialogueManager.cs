8f76a773f5ac1537c24bfa4af5de305d204694c6
Emerson Tanno <tannobrizotto@gmail.com>
2026-01-10 10:48:02 -0300

    :construction: feat(DialogueManager): pass dialogues

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -14,6 +14,11 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI RightText;
 
 
+    public bool dialogueActive = false;
+    private bool isTyping;
+    private bool nextLine;
+    private bool completeLine;
+
     void Awake()
     {
         if(Instance && Instance != this)
@@ -26,16 +31,7 @@ public class DialogueManager : MonoBehaviour
 
     void Start()
     {
-        SetDialogueTest();
-    }
-
-    private void SetDialogueTest()
-    {
-        List<DialogueLine> dialogue = JsonManager.Instance.GetDialogue(1, 1);
-
-        SetDialogueCanvas(true);
-        SetDialogueSide(dialogue[0].portrait);
-        StartCoroutine(DisplayDialogueLine(dialogue[0].textLinePt));
+        SetDialogue(1, 1);
     }
 
     private void SetDialogueCanvas(bool active)
@@ -59,13 +55,101 @@ public class DialogueManager : MonoBehaviour
 
     private IEnumerator DisplayDialogueLine(string dialogueLine)
     {
-        string line = "";
-        foreach(char character in dialogueLine)
+        isTyping = true;
+        RightText.text = "";
+
+        foreach (char character in dialogueLine)
         {
-            line += character;
-            RightText.text = line;
-            yield return new WaitForSecondsRealtime(0.03f);
+            RightText.text += character;
+            if(!completeLine)
+                yield return new WaitForSecondsRealtime(0.03f);
+        }
+
+        yield return new WaitForSecondsRealtime(0.1f);
+        completeLine = false;
+        isTyping = false;
+    }
+
+
+    public void SetDialogue(int npcId, int dialogueId)
+    {
+        SetDialogueCanvas(true);
+        dialogueActive = true;
+
+        List<DialogueLine> dialogue = JsonManager.Instance.GetDialogue(npcId, dialogueId);
+
+        StartCoroutine(StartDialogue(dialogue));
+    }
+
+    private IEnumerator StartDialogue(List<DialogueLine> dialogue)
+    {
+        foreach (DialogueLine dialogueLine in dialogue)
+        {
+            SetDialogueSide(dialogueLine.portrait);
+
+            string textLine = GetLanguageLine(
+                GameConfigurations.Instance.gameLanguage,
+                dialogueLine
+            );
+
+            yield return StartCoroutine(DisplayDialogueLine(textLine));
+
+            while (!nextLine)
+            {
+                yield return null;
+            }
+
+            nextLine = false;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+
+        while (!nextLine)
+        {
+            yield return null;
+        }
+
+        nextLine = false;
+        
+        EndDialogue();
+    }
+
+
+    private void EndDialogue()
+    {
+        dialogueActive = false;
+        SetDialogueCanvas(false);
+    }
+
+    private string GetLanguageLine(LanguageEnum language, DialogueLine line)
+    {
+        switch(language)
+        {
+            case LanguageEnum.Potugues:
+                {
+                    return line.textLinePt;
+                }
+            
+            case LanguageEnum.Ingles:
+                {
+                    return line.textLineEn;
+                }
+            default:
+                {
+                    return "";
+                }
         }
     }
 
+    public void NextLine()
+    {
+        if (!dialogueActive) return;
+
+        if (isTyping)
+        {
+            completeLine = true;
+            return;
+        }
+
+        nextLine = true;
+    }
 }
