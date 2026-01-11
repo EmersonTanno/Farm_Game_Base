e43cb7f5b3e2fa206dcac8bbfaa6d0e56fc75d8a
Emerson Tanno <tannobrizotto@gmail.com>
2026-01-11 17:41:58 -0300

    :construction: feat(DialogueManager): starting ramification dialogue

--- Assets/Scripts/Dialogue/types/DialogueLine.cs
+++ Assets/Scripts/Dialogue/types/DialogueLine.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 [System.Serializable]
 public class DialogueLine
 {
     public int portrait;
     public string textLinePt;
     public string textLineEn;
+    public List<DialogueOptions> options;
 }
---
+++ Assets/Scripts/Dialogue/types/DialogueOptions.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class DialogueOptions
+{
+    public string answerOptionPt;
+    public string answerOptionEn;
+    public int toDialogueId;
+}
