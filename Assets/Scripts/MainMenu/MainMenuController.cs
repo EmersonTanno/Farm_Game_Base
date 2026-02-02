66e64bb9416a8b062a458504d05945ef37884c1a
Emerson Tanno <tannobrizotto@gmail.com>
2026-02-02 09:37:16 -0300

    :sparkles: feat(MainMenuController / HomeScreen): adding new game intro

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,23 +1,101 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] GameObject buttons;
     [SerializeField] string newGameScene;
     [SerializeField] string loadGameScene;
+
+
+    #region new game variables
+    private bool startNewGame = false;
+    [SerializeField] private RectTransform textTransform;
+    [SerializeField] private float scrollSpeed = 30f;
+    [SerializeField] private float endY = 800f;
+    [SerializeField] private Image backGroundIntro;
+    [SerializeField] private GameObject backGroundIntroObj;
+    [SerializeField] private float fadeSpeed = 1f;
+    private float bgAlpha = 0f;
+    private bool canSkipIntro = false;
+    private bool skipIntro = false;
+    #endregion
+
+
+    #region Default
+    void Start()
+    {
+        backGroundIntroObj.SetActive(false);
+        SetBackgroundAlpha(0f);
+    }
+
+    void Update()
+    {
+        if (!startNewGame) return;
+
+        backGroundIntroObj.SetActive(true);
+
+        if (bgAlpha < 1f)
+        {
+            bgAlpha += fadeSpeed * Time.deltaTime;
+            SetBackgroundAlpha(bgAlpha);
+        }
+        else
+        {
+            canSkipIntro = true;
+            if(skipIntro)
+            {
+                SceneManager.LoadScene(newGameScene);
+            }
+            buttons.SetActive(false);
+
+            textTransform.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+
+            if (textTransform.anchoredPosition.y >= endY)
+            {
+                SceneManager.LoadScene(newGameScene);
+            }
+        }
+    }
+    #endregion
+
+    #region Start/Load
     public void StartNewGame()
     {
+        if(startNewGame) return;
         BootContext.IsLoadingGame = false;
         BootContext.SaveSlot = null;
-        SceneManager.LoadScene(newGameScene);
+        startNewGame = true;
     }
 
     public void LoadGame()
     {
+        if(startNewGame) return;
         BootContext.IsLoadingGame = true;
         BootContext.SaveSlot = "1";
         SceneManager.LoadScene(loadGameScene);
     }
+    #endregion
+
+    #region Auxiliar
+    private void SetBackgroundAlpha(float alpha)
+    {
+        Color c = backGroundIntro.color;
+        c.a = Mathf.Clamp01(alpha);
+        backGroundIntro.color = c;
+    }
+    #endregion
+
+    #region Input
+    public void Skip()
+    {
+        if(!startNewGame || !canSkipIntro) return;
+
+        skipIntro = true;
+    }
+    #endregion
 }
