44b15779ddd25b37efe2e5232b07715a63249554
Emerson Tanno <tannobrizotto@gmail.com>
2026-03-05 14:45:57 -0300

    :construction: wip(weather): initiating snow and blizzard weather

--- Assets/Scripts/Weather/WeatherEnum.cs
+++ Assets/Scripts/Weather/WeatherEnum.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public enum WeatherEnum
 {
     SUN,
     CLOUD,
     RAIN,
-    TEMPEST
+    TEMPEST,
+    SNOW,
+    BLIZZARD
 }
